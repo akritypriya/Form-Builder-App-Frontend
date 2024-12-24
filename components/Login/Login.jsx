33f717ed5d1ccb55d84994db94992b80564c6509
Akrity Priya <akritipriya1905@gamil.com>
2024-12-25 02:51:46 +0530

    feat: register and login CSS

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,146 +1,111 @@
-// import { login } from '../../services'
-// import { useEffect, useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import styles from './Login.module.css'
-// export default function Login() {
-//     const navigate = useNavigate()
-//     useEffect(() => {
-//         const token = localStorage.getItem('token')
-//         if (token) {
-//             navigate('/workspace')
-//         }
-//     }, [])
-//     const [loginformData, setLoginformData] = useState({
-//         email: '',
-//         password: '',
-//     })
-//     const handleLogin = async (e) => {
-//         e.preventDefault()
-//         const res = await login(loginformData)
-//         if (res.status === 200) {
-//             const data = await res.json()
-//             console.log(data)
-//             localStorage.setItem('token', data.token)
-//             alert('logged in successfully')
-//             navigate('/workspace')
-//         }
-//         else {
-//             console.log(res)
-//             alert('error')
-//         }
-//     }
-//     return (
-//         <div className={styles.header}>
-//         <div className={styles.login}>
-//         <form onSubmit={handleLogin}>
-//             <div className={styles.formText}>
-//             <label htmlFor="email">Email</label>   
-//             <input type="email" onChange={(e) => setLoginformData({ ...loginformData, [e.target.name]: e.target.value })} value={loginformData.email} name="email" placeholder="Enter your email" />
-//             </div><br />
-//             <div className={styles.formPassword}>
-//             <label htmlFor="password">Password</label>   
-//             <input type="password" onChange={(e) => setLoginformData({ ...loginformData, [e.target.name]: e.target.value })} value={loginformData.password} name="password" placeholder="**********" />
-//             </div><br />
-//             <div className={styles.formButton1}>
-//                                     <button type="submit">Sign Up</button>
-//                                 </div>
-//                                 <br />
-//                             </form>
-//                             <p>OR</p>
-//                             <div className={styles.formButton2}>
-//                                 <button >Sign In with Google</button>
-//                             </div>
-//                             <p>Don’t have an account? Register now</p>
-//                         </div>
-//                     </div>
-//     )
-// }
-import { login } from '../../services';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Login.module.css';
+import { login } from "../../services";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
+import arrow_back from "../../assets/arrow_back.png";
+import down_ellipse from "../../assets/down_ellipse.png";
+import Google_Icon from "../../assets/Google_Icon.svg";
+import side_ellipse from "../../assets/side_ellipse.png";
+import triangle from "../../assets/triangle.png";
 
 export default function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // Redirect user if already logged in
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            navigate('/workspace');
-        }
-    }, [navigate]);
+  // Redirect user if already logged in
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/workspace");
+    }
+  }, [navigate]);
 
-    const [loginformData, setLoginformData] = useState({
-        email: '',
-        password: '',
-    });
+  const [loginformData, setLoginformData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        setError(''); // Clear previous errors
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(""); // Clear previous errors
 
-        try {
-            const res = await login(loginformData);
-            if (res.status === 200) {
-                const data = await res.json();
-                localStorage.setItem('token', data.token);
-                alert('Logged in successfully');
-                navigate('/workspace');
-            } else {
-                setError('Invalid email or password'); // Handle non-200 response
-            }
-        } catch (err) {
-            console.error(err);
-            setError('An error occurred. Please try again.'); // Handle network or server errors
-        }
-    };
+    try {
+      const res = await login(loginformData);
+      if (res.status === 200) {
+        const data = await res.json();
+        localStorage.setItem("token", data.token);
+        alert("Logged in successfully");
+        navigate("/workspace");
+      } else {
+        setError("Invalid email or password"); // Handle non-200 response
+      }
+    } catch (err) {
+      console.error(err);
+      setError("An error occurred. Please try again."); // Handle network or server errors
+    }
+  };
 
-    return (
-        <div className={styles.header}>
-            <div className={styles.login}>
-                <form onSubmit={handleLogin}>
-                    <div className={styles.formText}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Enter your email"
-                            value={loginformData.email}
-                            onChange={(e) =>
-                                setLoginformData({ ...loginformData, email: e.target.value })
-                            }
-                        />
-                    </div>
-                    <div className={styles.formPassword}>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="**********"
-                            value={loginformData.password}
-                            onChange={(e) =>
-                                setLoginformData({ ...loginformData, password: e.target.value })
-                            }
-                        />
-                    </div>
-                    {error && <p className={styles.error}>{error}</p>} {/* Display error message */}
-                    <div className={styles.formButton1}>
-                        <button type="submit">Log In</button>
-                    </div>
-                </form>
-                <p>OR</p>
-                <div className={styles.formButton2}>
-                    <button type="button">Sign In with Google</button>
-                </div>
-                <p>
-                    Don’t have an account? <a href="/register">Register now</a>
-                </p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.header}>
+      <a href="/home">
+        <img src={arrow_back} alt="back_button" className={styles.backButton} />
+      </a>
+      <div>
+        <img src={triangle} alt="triangle" className={styles.triangle} />
+        <img src={side_ellipse} alt="side_icon" className={styles.sideIcon} />
+      </div>
+      <div className={styles.login}>
+        <form onSubmit={handleLogin}>
+          <div className={styles.formText}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={loginformData.email}
+              onChange={(e) =>
+                setLoginformData({ ...loginformData, email: e.target.value })
+              }
+            />
+          </div>
+          <div className={styles.formPassword}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="**********"
+              value={loginformData.password}
+              onChange={(e) =>
+                setLoginformData({ ...loginformData, password: e.target.value })
+              }
+            />
+            {error && <p className={styles.error}>{error}</p>}{" "}
+            {/* Display error message */}
+          </div>
+
+          <button type="submit" className={styles.formButton1}>
+            Log In
+          </button>
+        </form>
+        <p className={styles.textOR}>OR</p>
+
+        <button type="button" className={styles.formButton2}>
+          <img src={Google_Icon} alt="icon" className={styles.GoogleIcon} />
+          Sign in with Google
+        </button>
+
+        <p className={styles.text}>
+          Don’t have an account?
+          <a href="/register" className={styles.link}>
+            Register now
+          </a>
+        </p>
+
+        <img src={down_ellipse} alt="down_icon" className={styles.downIcon} />
+      </div>
+    </div>
+  );
 }
--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -1,161 +1,157 @@
-// import { register } from '../../services'
-// import { useState } from 'react'
-// import styles from './Register.module.css';
-
-// export default function Register() {
-//     const [formData, setFormData] = useState({
-//         username: '',   
-//         email: '',
-//         password: '',
-//         confirmPassword: '',
-//     })
-    
-//     const [error, setError] = useState('');
-
-//     const handleRegister = async (e) => {
-//         e.preventDefault() 
-//         const { username, email, password, confirmPassword } = formData;
-
-//                 // Password match validation
-//                 if (password !== confirmPassword) {
-//                     setError('Passwords do not match');
-//                     return;
-//                 }
-//                 try {
-//                     const res = await register(formData);
-        
-//                     if (res.status === 200) {
-//                         alert('Registered successfully');
-//                         setFormData({ username: '', email: '', password: '', confirmPassword: '' }); // Reset form
-//                         setError(''); // Clear error
-//                     } else {
-//                         setError(res.data.message || 'Registration failed');
-//                     }
-//                 } catch (err) {
-//                     console.error(err);
-//                     setError('An error occurred. Please try again.');
-//                 }
-//             };
-    
-//     return (
-//         <div className={styles.header}>
-//            <div className={styles.form}>
-//         <form onSubmit={handleRegister}>
-//             <div className={styles.formText}><input type="text" onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.name} name="username" placeholder="Enter a username" /></div><br />
-//             <div className={styles.formText}><input type="text" onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.email} name="email" placeholder="Enter  your email" /></div><br />
-//             <div className={styles.formPassword}><input type="password" onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.password} name="password" placeholder="***********" /></div><br />   
-//             <div className={styles.formPassword}><input type="password" onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })} value={formData.confirmPassword} name="confirmPassword" placeholder="***********" /></div><br />         
-//             <div className={styles.formButton1}><button type="submit">Sign Up</button></div><br />
-//         </form>
-//         <p>OR</p>
-//         <div className={styles.formButton2}><button type="submit">Sign In with Google</button></div>
-//         <p>Don’t have an account? Register now</p>
-//         </div> 
-//         </div>
-//     )
-// }
-import { register } from '../../services';
-import { useState } from 'react';
-import styles from './Register.module.css';
+import { register } from "../../services";
+import { useState } from "react";
+import styles from "./Register.module.css";
+import arrow_back from "../../assets/arrow_back.png";
+import down_ellipse from "../../assets/down_ellipse.png";
+import Google_Icon from "../../assets/Google_Icon.svg";
+import side_ellipse from "../../assets/side_ellipse.png";
+import triangle from "../../assets/triangle.png";
 
 export default function Register() {
-    const [formData, setFormData] = useState({
-        username: '',   
-        email: '',
-        password: '',
-        confirmPassword: '',
-    });
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        const { username, email, password, confirmPassword } = formData;
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const { username, email, password, confirmPassword } = formData;
 
-        // Password match validation
-        if (password !== confirmPassword) {
-            setError('enter same password in both fields');
-            return;
-        }
+    // Password match validation
+    if (password !== confirmPassword) {
+      setError("enter same password in both fields");
 
-        try {
-            const res = await register(formData);
+      return;
+    }
 
-            if (res.status === 200) {
-                alert('Registered successfully');
-                setFormData({ username: '', email: '', password: '', confirmPassword: '' }); // Reset form
-                setError(''); // Clear error
-            } else {
-                setError(res.data.message || 'Registration failed');
-            }
-        } catch (err) {
-            console.error(err);
-            setError('An error occurred. Please try again.');
-        }
-    };
+    try {
+      const res = await register(formData);
 
-    return (
-        <div className={styles.header}>
-            <div className={styles.form}>
-                <form onSubmit={handleRegister}>
-                    <div className={styles.formText}>
-                    <label htmlFor="username">Username</label>
-                        <input 
-                            type="text" 
-                            name="username" 
-                            placeholder="Enter your username" 
-                            value={formData.username} 
-                            onChange={(e) => setFormData({ ...formData, username: e.target.value })} 
-                        />
-                    </div>
-                    <br />
-                    <div className={styles.formText}>
-                    <label htmlFor="email">Email</label>
-                        <input 
-                            type="email" 
-                            name="email" 
-                            placeholder="Enter your email" 
-                            value={formData.email} 
-                            onChange={(e) => setFormData({ ...formData, email: e.target.value })} 
-                        />
-                    </div>
-                    <br />
-                    <div className={styles.formPassword}>
-                    <label htmlFor="password">Password</label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            placeholder="**********" 
-                            value={formData.password} 
-                            onChange={(e) => setFormData({ ...formData, password: e.target.value })} 
-                        />
-                    </div>
-                    <br />
-                    <div className={styles.formPassword}>
-                    <label htmlFor="password">Confirm Password</label>
-                        <input 
-                            type="password" 
-                            name="confirmPassword" 
-                            placeholder="*********" 
-                            value={formData.confirmPassword} 
-                            onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })} 
-                        />
-                    </div>
-                    <br />
-                    {error && <p className={styles.error}>{error}</p>}
-                    <div className={styles.formButton1}>
-                        <button type="submit">Sign Up</button>
-                    </div>
-                    <br />
-                </form>
-                <p>OR</p>
-                <div className={styles.formButton2}>
-                    <button >Sign In with Google</button>
-                </div>
-                <p>Already have an account?
-                <a href="/login">Login</a>
-                </p>
-            </div>
-        </div>
-    );
+      if (res.status === 200) {
+        alert("Registered successfully");
+        setFormData({
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }); // Reset form
+        setError(""); // Clear error
+      } else {
+        setError(res.data.message || "Registration failed");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className={styles.header}>
+      <a href="/home">
+        <img src={arrow_back} alt="back_button" className={styles.backButton} />
+      </a>
+      <div>
+        <img src={triangle} alt="triangle" className={styles.triangle} />
+        <img src={side_ellipse} alt="side_icon" className={styles.sideIcon} />
+      </div>
+
+      <div className={styles.form}>
+        <form onSubmit={handleRegister}>
+          <div className={styles.formText}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+              value={formData.username}
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
+            />
+          </div>
+
+          <div className={styles.formText}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+            />
+          </div>
+
+          <div className={styles.formPassword}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="**********"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
+          </div>
+
+          <div
+            className={`${styles.formPassword} ${
+              formData.password &&
+              formData.confirmPassword &&
+              formData.password !== formData.confirmPassword
+                ? styles.errorBorder
+                : ""
+            }`}
+          >
+            <label
+              htmlFor="confirmPassword"
+              className={`${
+                formData.password &&
+                formData.confirmPassword &&
+                formData.password !== formData.confirmPassword
+                  ? styles.errorLabel
+                  : ""
+              }`}
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="**********"
+              value={formData.confirmPassword}
+              onChange={(e) =>
+                setFormData({ ...formData, confirmPassword: e.target.value })
+              }
+            />
+            {error && <p className={styles.error}>{error}</p>}
+          </div>
+          <button type="submit" className={styles.formButton1}>
+            Sign Up
+          </button>
+        </form>
+        <p className={styles.textOR}>OR</p>
+
+        <button type="button" className={styles.formButton2}>
+          <img src={Google_Icon} alt="icon" className={styles.GoogleIcon} />
+          Sign Up with Google
+        </button>
+
+        <p className={styles.text}>
+          Already have an account?
+          <a href="/login" className={styles.link}>
+            Login
+          </a>
+        </p>
+
+        <img src={down_ellipse} alt="down_icon" className={styles.downIcon} />
+      </div>
+    </div>
+  );
 }
